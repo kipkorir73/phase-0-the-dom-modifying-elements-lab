c7bcebf265d86733746335c288decc4a1ab1bd9d
collins <kipkorirc583@gmail.com>
2023-07-07 01:39:59 +0300

    fisr

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+// Get the main element
+const mainElement = document.getElementById('main');
+
+// Remove the main element
+mainElement.remove();
+
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the newHeader to the document body
+document.body.appendChild(newHeader);
+
+// Export the newHeader variable for testing
+module.exports = {
+  newHeader
+};
